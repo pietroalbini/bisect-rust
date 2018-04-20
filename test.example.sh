0c0e378c821639e23a6b75e19b4c2ff70f5cf75f
Pietro Albini <pietro@pietroalbini.org>
2018-04-20 10:32:42 +0200

    Improve the example test script

--- test.example.sh
+++ test.example.sh
@@ -4,9 +4,21 @@ set -eu
 TEST_DIRECTORY="test"
 ERROR_MESSAGE="internal compiler error"
 CARGO_COMMAND="test"
+DEBUG=false
+SEARCH_FIX=false
 
 cargo_absolute="$(readlink -e "${CARGO_RELATIVE}")"
 
 cd "${TEST_DIRECTORY}"
 rm -rf target
-"${cargo_absolute}" "${CARGO_COMMAND}" 2>&1 | grep -q "${ERROR_MESSAGE}"
+out=$("${cargo_absolute}" ${CARGO_COMMAND} 2>&1 || true)
+"${DEBUG}" && echo "${out}"
+
+if "${SEARCH_FIX}"; then
+    if echo "${out}" | grep -q "${ERROR_MESSAGE}"; then
+        exit 1
+    fi
+    exit 0
+fi
+
+echo "${out}" | grep -q "${ERROR_MESSAGE}"
